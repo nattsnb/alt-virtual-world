015a3017548d5ef71f0ea9de2c6884a724773331
Natasza ASzatan <natiye.nb@gmai.com>
2024-05-02 08:45:36 +0100

    run prettier

--- src/Organism.js
+++ src/Organism.js
@@ -1,16 +1,16 @@
 export class Organism {
-    constructor(board, startParameters) {
-        this.age = startParameters.age;
-        this.image = startParameters.image;
-        this.initiative = startParameters.initiative;
-        this.element = document.createElement('span');
-        this.element.innerText = 'organism';
-        this.board = board;
-    }
+  constructor(board, startParameters) {
+    this.age = startParameters.age;
+    this.image = startParameters.image;
+    this.initiative = startParameters.initiative;
+    this.element = document.createElement('span');
+    this.element.innerText = 'organism';
+    this.board = board;
+  }
 
-    createElement = () => {
-        this.element = document.createElement('img');
-        this.element.classList.add('organism-image');
-        this.element.src = this.image;
-    };
+  createElement = () => {
+    this.element = document.createElement('img');
+    this.element.classList.add('organism-image');
+    this.element.src = this.image;
+  };
 }
