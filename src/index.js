7c199b5cefdd8bc2ee46a820421426e287997340
Natasza ASzatan <natiye.nb@gmai.com>
2024-05-05 22:48:34 +0100

    set delay before the rounds to check if works

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ await board.round();
 // console.log(board.tiles[1][2]
 
 let game = 0;
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 while (game < 15) {
+  await delay(2000);
   await board.round();
   game = game + 1;
   console.log(game);
