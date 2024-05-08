a74156c3039e73b8a89743e2c17a0a2e510a4a80
Natasza ASzatan <natiye.nb@gmai.com>
2024-05-08 14:05:49 +0100

    added eventlistner to tiles, it produces new character on board

--- src/Tile.js
+++ src/Tile.js
@@ -1,3 +1,5 @@
+import { classesList } from './classesList';
+
 export class Tile {
   constructor(x, y, board) {
     this.x = x;
@@ -5,6 +7,7 @@ export class Tile {
     this.currentOrganism = null;
     this.createTileElement();
     this.board = board;
+    this.initializeEventListener();
   }
 
   createTileElement = () => {
@@ -26,5 +29,22 @@ export class Tile {
   clearTile = () => {
     this.currentOrganism = null;
     this.tileContainer.innerHTML = '';
+  };
+
+  initializeEventListener = () => {
+    this.tileContainer.addEventListener('click', this.checkClick);
+  };
+
+  checkClick = () => {
+    if (!this.currentOrganism) {
+      let RandomOrganismClass =
+        classesList[Math.floor(Math.random() * classesList.length)];
+      const organism = new RandomOrganismClass(
+        this.board,
+        RandomOrganismClass.startParameters,
+      );
+      console.log(organism);
+      this.addCurrentOrganism(organism);
+    }
   };
 }
